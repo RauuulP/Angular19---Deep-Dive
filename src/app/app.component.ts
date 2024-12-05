067dbceae3bf04f0feed3304c30d3fa9698c5c14
RauuulP <raul.pacurar@accesa.eu>
2024-12-05 12:59:01 +0200

    section 1 - components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,18 +30,6 @@ export class AppComponent implements AfterViewInit {
     // console.log(this.cards)
   }
 
-  onCoursesEdited() {
-    this.courses.push({
-      id: 1,
-      description: "Angular core deep dive",
-      iconUrl:
-        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png",
-      longDescription:
-        "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-      category: "INTERMEDIATE",
-      lessonsCount: 10,
-    });
-  }
 
   onCourseSelected(course: Course) {}
 }
